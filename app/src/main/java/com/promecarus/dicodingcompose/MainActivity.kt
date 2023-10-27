e63c3b89b6fc38dde7e36d88ba57ffc6a0fc18ba
promecarus <haikalslipi@gmail.com>
2023-10-27 21:58:47 +0700

    part 04

--- app/src/main/java/com/promecarus/dicodingcompose/MainActivity.kt
+++ app/src/main/java/com/promecarus/dicodingcompose/MainActivity.kt
@@ -3,7 +3,11 @@ package com.promecarus.dicodingcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.outlined.ExpandMore
@@ -66,13 +72,18 @@ fun DicodingComposeApp() {
 
 @Composable
 fun GreetingList(names: List<String>) {
-    if (names.isNotEmpty()) Column { names.forEach { Greeting(it) } }
-    else Text("No people to greet :(")
+    if (names.isNotEmpty()) LazyColumn { items(names) { Greeting(it) } }
+    else Box(contentAlignment = Alignment.Center) { Text("No people to greet :(") }
 }
 
 @Composable
 fun Greeting(name: String) {
     var isExpanded by remember { mutableStateOf(false) }
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp, animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
+        ), label = ""
+    )
 
     Row(
         modifier = Modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically
@@ -80,7 +91,7 @@ fun Greeting(name: String) {
         Image(
             painter = painterResource(R.drawable.jetpack_compose),
             contentDescription = "Logo Jetpack Compose",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.size(animatedSizeDp)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column(modifier = Modifier.weight(1f)) {
